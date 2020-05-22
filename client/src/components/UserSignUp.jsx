1b41ec3cfc87548acc680bac89ceda8c12ccef59
Rae <raefeldman@gmail.com>
2020-05-21 17:48:44 -0700

    updated UserSignUp page and added validation errors

--- client/src/components/UserSignUp.jsx
+++ client/src/components/UserSignUp.jsx
@@ -1,13 +1,47 @@
 import React from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 class UserSignUp extends React.Component {
+  state = {
+    firstName: "",
+    lastName: "",
+    emailAddress: "",
+    password: "",
+    confirmPassword: "",
+    errors: null,
+  };
   render() {
     return (
       <div className="bounds">
         <div className="grid-33 centered signin">
           <h1>Sign Up</h1>
+          {this.state.errors ? (
+            <div>
+              <h2 className="validation--errors--label">Validation errors</h2>
+              <div className="validation-errors">
+                <ul>
+                  {this.state.errors.map((error) => (
+                    <li key={error}>{error}</li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          ) : (
+            <div></div>
+          )}
           <div>
-            <form>
+            <form
+              onSubmit={(e) => {
+                this.handleSubmit(
+                  e,
+                  this.state.firstName,
+                  this.state.lastName,
+                  this.state.emailAddress,
+                  this.state.password
+                );
+              }}
+            >
               <div>
                 <input
                   id="firstName"
@@ -15,7 +49,8 @@ class UserSignUp extends React.Component {
                   type="text"
                   className=""
                   placeholder="First Name"
-                  value=""
+                  value={this.state.firstName}
+                  onChange={this.change}
                 />
               </div>
               <div>
@@ -25,7 +60,8 @@ class UserSignUp extends React.Component {
                   type="text"
                   className=""
                   placeholder="Last Name"
-                  value=""
+                  value={this.state.lastName}
+                  onChange={this.change}
                 />
               </div>
               <div>
@@ -35,7 +71,8 @@ class UserSignUp extends React.Component {
                   type="text"
                   className=""
                   placeholder="Email Address"
-                  value=""
+                  value={this.state.emailAddress}
+                  onChange={this.change}
                 />
               </div>
               <div>
@@ -45,7 +82,8 @@ class UserSignUp extends React.Component {
                   type="password"
                   className=""
                   placeholder="Password"
-                  value=""
+                  value={this.state.password}
+                  onChange={this.change}
                 />
               </div>
               <div>
@@ -55,23 +93,20 @@ class UserSignUp extends React.Component {
                   type="password"
                   className=""
                   placeholder="Confirm Password"
-                  value=""
+                  value={this.state.confirmPassword}
+                  onChange={this.change}
                 />
               </div>
               <div className="grid-100 pad-bottom">
                 <button className="button" type="submit">
                   Sign Up
                 </button>
-                <button
-                  className="button button-secondary"
-                  onclick="event.preventDefault(); location.href='index.html';"
-                >
+                <Link className="button button-secondary" to="/">
                   Cancel
-                </button>
+                </Link>
               </div>
             </form>
           </div>
-          <p>&nbsp;</p>
           <p>
             Already have a user account? <a href="sign-in.html">Click here</a>{" "}
             to sign in!
@@ -80,6 +115,39 @@ class UserSignUp extends React.Component {
       </div>
     );
   }
+
+  change = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    this.setState(() => {
+      return {
+        [name]: value,
+      };
+    });
+  };
+
+  handleSubmit = (e, firstName, lastName, emailAddress, password) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:5000/api/users", {
+        firstName: firstName,
+        lastName: lastName,
+        emailAddress: emailAddress,
+        password: password,
+      })
+      .catch((error) => {
+        if (error.response.data.errorMessages) {
+          this.setState({ errors: error.response.data.errorMessages });
+        }
+
+        if (error.response.data.message) {
+          let errors = [];
+          errors.push(error.response.data.message);
+          this.setState({ errors: errors });
+        }
+      });
+  };
 }
 
 export default UserSignUp;
